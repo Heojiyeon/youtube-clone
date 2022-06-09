479035b08f366a7db61801ebd78f92a5eee2e4fb
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2022-06-09 14:27:31 +0900

    Save sessions to db

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { urlencoded } from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
@@ -19,9 +20,10 @@ app.use(urlencoded({ extended: true }));
 
 app.use(
     session({
-        secret: "Hello!",
-        resave: true,
-        saveUninitialized: true,
+        secret: process.env.COOKIE_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL })
     })
 );
 
